58b94a80a14ec959bae359502c629fa0be602010
Tung Bui <tung.bquang@gmail.com>
2023-05-05 16:37:47 +0700

    Fix issue cannot create IAM role

--- serverless/main.tf
+++ serverless/main.tf
@@ -12,12 +12,28 @@ provider "aws" {
 }
 
 # IAM role for Lambda
+
 resource "aws_iam_role" "lamda_apigateway_role" {
   name = "lamda_apigateway_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# IAM policy
+resource "aws_iam_policy" "lamda_apigateway_policy" {
+  name = "lamda_apigateway_policy"
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
+  policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
@@ -43,8 +59,9 @@ resource "aws_iam_role" "lamda_apigateway_role" {
       }
     ]
   })
+}
 
-  tags = {
-    tag-key = "serverless_lab"
-  }
+resource "aws_iam_role_policy_attachment" "lamda_apigateway_role_policy" {
+  policy_arn = aws_iam_policy.lamda_apigateway_policy.arn
+  role = aws_iam_role.lamda_apigateway_role.name
 }
