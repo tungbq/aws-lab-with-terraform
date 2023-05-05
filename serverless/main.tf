54cd730a7ec8ca60210ecb3d015d0a0339e7f040
Tung Bui <tung.bquang@gmail.com>
2023-05-05 16:51:37 +0700

    create function + add test data

--- serverless/main.tf
+++ serverless/main.tf
@@ -65,3 +65,28 @@ resource "aws_iam_role_policy_attachment" "lamda_apigateway_role_policy" {
   policy_arn = aws_iam_policy.lamda_apigateway_policy.arn
   role = aws_iam_role.lamda_apigateway_role.name
 }
+
+# Create Lambda function
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "lambda_http.py"
+  output_path = "lambda_http_payload.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "lambda_http_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.lamda_apigateway_role.arn
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "python3.8"
+  handler = "lambda_http.lambda_handler"
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
