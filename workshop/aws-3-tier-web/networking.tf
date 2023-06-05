b25d363028a6cdc4b9b03ee9c6bf117e12152c84
Tung Bui Quang (Leo) <tung.bquang@gmail.com>
2023-06-05 19:16:20 +0700

    workshop: add VPC with 6 subnets

---
+++ workshop/aws-3-tier-web/networking.tf
@@ -0,0 +1,86 @@
+
+provider "aws" {
+  region = "us-east-1" # Replace with your desired region
+}
+
+resource "aws_vpc" "workshop_aws_3_tier_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
+### Web tier subnets
+resource "aws_subnet" "public_web_subnet_1a" {
+  vpc_id                  = aws_vpc.workshop_aws_3_tier_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a" # Replace with your desired availability zone
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public-Web-Subnet-AZ-1A"
+  }
+}
+
+resource "aws_subnet" "public_web_subnet_1b" {
+  vpc_id                  = aws_vpc.workshop_aws_3_tier_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b" # Replace with your desired availability zone
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public-Web-Subnet-AZ-1B"
+  }
+}
+
+
+### App tier subnets
+resource "aws_subnet" "private_app_subnet_1a" {
+  vpc_id                  = aws_vpc.workshop_aws_3_tier_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1a" # Replace with your desired availability zone
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Private-App-Subnet-AZ-1A"
+  }
+}
+
+resource "aws_subnet" "private_app_subnet_1b" {
+  vpc_id                  = aws_vpc.workshop_aws_3_tier_vpc.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1b" # Replace with your desired availability zone
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Private-App-Subnet-AZ-1B"
+  }
+}
+
+
+### DB tier subnets
+resource "aws_subnet" "private_db_subnet_1a" {
+  vpc_id                  = aws_vpc.workshop_aws_3_tier_vpc.id
+  cidr_block              = "10.0.5.0/24"
+  availability_zone       = "us-east-1a" # Replace with your desired availability zone
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Private-DB-Subnet-AZ-1A"
+  }
+}
+
+resource "aws_subnet" "private_db_subnet_1a" {
+  vpc_id                  = aws_vpc.workshop_aws_3_tier_vpc.id
+  cidr_block              = "10.0.6.0/24"
+  availability_zone       = "us-east-1b" # Replace with your desired availability zone
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Private-DB-Subnet-AZ-1B"
+  }
+}
