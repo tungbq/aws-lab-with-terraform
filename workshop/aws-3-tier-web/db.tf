02b09a537c63606cefc423f2ced05e547ff0e7b2
Tung Bui <tung.bquang@gmail.com>
2023-06-10 12:50:02 +0700

    db: adding subnet group

--- workshop/aws-3-tier-web/db.tf
+++ workshop/aws-3-tier-web/db.tf
@@ -1,3 +1,10 @@
 # Subnet Group
-
+resource "aws_db_subnet_group" "three_tier_db_subnet_group" {
+  name       = "three_tier_db_subnet_group"
+  vpc_id = "${aws_vpc.workshop_aws_3_tier_vpc.id}"
+  subnet_ids = [aws_subnet.private_db_subnet_1a.id, aws_subnet.private_db_subnet_1b.id]
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
 # DB
