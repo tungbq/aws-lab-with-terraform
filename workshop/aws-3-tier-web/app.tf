86352fb7d40a4b185abb022485bf9356da462d17
Tung Bui <tung.bquang@gmail.com>
2023-06-14 21:31:51 +0700

    feat: updating AMI

--- workshop/aws-3-tier-web/app.tf
+++ workshop/aws-3-tier-web/app.tf
@@ -1,24 +1,8 @@
 # t2.micro node with an AWS Tag naming it "HelloWorld"
 
-## TODO: update this part
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
 
 resource "aws_instance" "app" {
-  ami           = "${data.aws_ami.amazon_linux.id}"
+  ami           = "ami-022e1a32d3f742bd8"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.private_app_subnet_1a.id
   tags = {
