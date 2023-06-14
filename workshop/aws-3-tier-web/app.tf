d4fbc5d50d29cc514fe317b5ee85ebe7d3b93d2b
Tung Bui <tung.bquang@gmail.com>
2023-06-14 10:15:41 +0700

    app: init instance(wip)

---
+++ workshop/aws-3-tier-web/app.tf
@@ -0,0 +1,27 @@
+# t2.micro node with an AWS Tag naming it "HelloWorld"
+
+## TODO: update this part
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app" {
+  ami           = "${data.aws_ami.amazon_linux.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
