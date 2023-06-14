efb7abd8e406de2a132a90ef746fc7cac8852ca5
Tung Bui <tung.bquang@gmail.com>
2023-06-14 22:03:46 +0700

    issue-102: fix iam issue

--- workshop/aws-3-tier-web/iam.tf
+++ workshop/aws-3-tier-web/iam.tf
@@ -1,3 +1,5 @@
+
+
 resource "aws_iam_role" "ec2_role_for_s3_ssm" {
   name = "ec2-role-for-s3-and-ssm"
   assume_role_policy = <<EOF
@@ -24,4 +26,8 @@ resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core_policy_atta
 resource "aws_iam_role_policy_attachment" "s3_readonly_policy_attachment" {
   role       = aws_iam_role.ec2_role_for_s3_ssm.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+resource "aws_iam_instance_profile" "ec2_role_for_s3_ssm_profile" {
+  name = "ec2_role_for_s3_ssm_profile"
+  role = aws_iam_role.ec2_role_for_s3_ssm.name
 }
