7d9b59c6589968e3beae3d0b8bd569d8e05728ff
Tung Bui <tung.bquang@gmail.com>
2023-06-14 21:38:41 +0700

    ec2: add iam role and security group

--- workshop/aws-3-tier-web/app.tf
+++ workshop/aws-3-tier-web/app.tf
@@ -5,7 +5,11 @@ resource "aws_instance" "app" {
   ami           = "ami-022e1a32d3f742bd8"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.private_app_subnet_1a.id
+  aws_iam_instance_profile = aws_iam_role.ec2_role_for_s3_ssm.name
+
+  security_groups = [aws_security_group.app_tier_sg.id]
+
   tags = {
-    Name = "HelloWorld"
+    Name = "AppLayer"
   }
 }
