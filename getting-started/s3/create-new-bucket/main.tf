1204e6cc38e962267b08693f7164126406d4105c
Tung Bui <tung.bquang@gmail.com>
2023-06-18 11:14:05 +0700

    [issue-97] s3: add content

--- getting-started/s3/create-new-bucket/main.tf
+++ getting-started/s3/create-new-bucket/main.tf
@@ -1,1 +1,12 @@
-# TODO
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired region
+}
+
+resource "aws_s3_bucket" "sample_s3_bucket" {
+  bucket = "sample-s3-tf"
+
+  tags = {
+    Name        = "S3 bucket to store code"
+    Environment = "Dev"
+  }
+}
