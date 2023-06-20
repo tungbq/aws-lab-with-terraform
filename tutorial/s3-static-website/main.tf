ecbb9a824f29e4ec61fe177bb0e555737bf33fad
Tung Bui <tung.bquang@gmail.com>
2023-06-20 23:09:58 +0700

    issue-131: Create new s3 bucket

--- tutorial/s3-static-website/main.tf
+++ tutorial/s3-static-website/main.tf
@@ -1,1 +1,12 @@
-# To be updated...
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired region
+}
+
+resource "aws_s3_bucket" "tungbq_s3_website" {
+  bucket = "tungbq-s3-website"
+
+  tags = {
+    Name        = "S3 bucket to store website"
+    Environment = "Dev"
+  }
+}
