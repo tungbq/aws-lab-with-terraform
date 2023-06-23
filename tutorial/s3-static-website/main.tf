b6b757ef2b03dd7aa8218b230be4a2c999a489cf
Tung Bui <tung.bquang@gmail.com>
2023-06-23 18:40:01 +0700

    issue-135: better naming

--- tutorial/s3-static-website/main.tf
+++ tutorial/s3-static-website/main.tf
@@ -56,11 +56,11 @@ POLICY
 }
 
 locals {
-  object_source = "${path.module}/asset/index.html"
+  index_html_source = "${path.module}/asset/index.html"
 }
 
 resource "aws_s3_object" "file_upload" {
   bucket      = "${aws_s3_bucket.tungbq_s3_website.bucket}"
   key         = "index.html"
-  source      = local.object_source
+  source      = local.index_html_source
 }
