7f39339d1f72ddb6e31f9a21a830387572e5ecef
Tung Bui <tung.bquang@gmail.com>
2023-06-29 22:16:54 +0700

    Init the amplify

--- tutorial/serverless-basic-application/main.tf
+++ tutorial/serverless-basic-application/main.tf
@@ -1,1 +1,40 @@
-# to be updated
+provider "aws" {
+  region = "us-east-1"  # Replace with your desired region
+}
+
+resource "aws_amplify_app" "example" {
+  name       = "example"
+  repository = "https://github.com/tungbq/source-serverless-basic-application"
+
+  build_spec = <<-EOT
+    version: 1
+    frontend:
+      phases:
+        pre_build:
+          commands:
+            - echo "Pre-build command"
+        build:
+          commands:
+            - echo "Build command"
+        post_build:
+          commands:
+            - echo "Post-build command"
+      artifacts:
+        baseDirectory: /
+        files:
+          - '**/*'
+      cache:
+        paths: []
+  EOT
+
+  # The default rewrites and redirects added by the Amplify Console.
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/index.html"
+  }
+
+  environment_variables = {
+    ENV = "test"
+  }
+}
