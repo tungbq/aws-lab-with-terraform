a881596c49a162b1fd4cad564aa5e477aac27e15
Tung Bui <tung.bquang@gmail.com>
2023-07-01 12:53:14 +0700

    Fix api gateway deployment.v1

--- tutorial/serverless-basic-application/api-gateway.tf
+++ tutorial/serverless-basic-application/api-gateway.tf
@@ -30,13 +30,32 @@ resource "aws_api_gateway_stage" "lambda" {
   }
 }
 
+# Resource
 resource "aws_api_gateway_resource" "resource" {
   path_part   = "resource"
   parent_id   = aws_api_gateway_rest_api.lambda.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.lambda.id
 }
 
+# Method
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.lambda.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
 
+# Integration
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.lambda.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.hello_world.invoke_arn
+}
+
+# Deployment
 resource "aws_api_gateway_deployment" "example" {
   rest_api_id = aws_api_gateway_rest_api.lambda.id
 
@@ -54,14 +73,7 @@ resource "aws_api_gateway_deployment" "example" {
   }
 }
 
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.lambda.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.hello_world.invoke_arn
-}
-
+# Logging
 resource "aws_cloudwatch_log_group" "api_gw" {
   name = "/aws/api_gw/${aws_api_gateway_rest_api.lambda.name}"
 
