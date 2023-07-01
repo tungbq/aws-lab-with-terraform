686f06fe5c4e55ac53ae0fab4ec054414ae8f5a2
Tung Bui <tung.bquang@gmail.com>
2023-07-01 12:18:28 +0700

    improve output

--- tutorial/serverless-basic-application/api-gateway.tf
+++ tutorial/serverless-basic-application/api-gateway.tf
@@ -39,7 +39,7 @@ resource "aws_apigatewayv2_integration" "hello_world" {
 resource "aws_apigatewayv2_route" "hello_world" {
   api_id = aws_apigatewayv2_api.lambda.id
 
-  route_key = "GET /hello"
+  route_key = "POST /"
   target    = "integrations/${aws_apigatewayv2_integration.hello_world.id}"
 }
 
--- tutorial/serverless-basic-application/outputs.tf
+++ tutorial/serverless-basic-application/outputs.tf
@@ -3,3 +3,9 @@ output "function_name" {
 
   value = aws_lambda_function.hello_world.function_name
 }
+
+output "base_url" {
+  description = "Base URL for API Gateway stage."
+
+  value = aws_apigatewayv2_stage.lambda.invoke_url
+}
