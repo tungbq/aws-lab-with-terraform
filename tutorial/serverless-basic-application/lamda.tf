acd5fc188eab919d5e9ad4b15b28fb54b84e2b69
Tung Bui <tung.bquang@gmail.com>
2023-07-01 11:26:07 +0700

    issue-161: fix missing role

--- tutorial/serverless-basic-application/lamda.tf
+++ tutorial/serverless-basic-application/lamda.tf
@@ -35,11 +35,33 @@ resource "aws_lambda_function" "hello_world" {
 
   source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
 
-  # role = aws_iam_role.lambda_exec.arn
+  role = aws_iam_role.lambda_exec.arn
 }
 
 resource "aws_cloudwatch_log_group" "hello_world" {
   name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
 
   retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
