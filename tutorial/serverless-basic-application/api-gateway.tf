44626b2489144568279ad882a94bc1d01583bee8
Tung Bui <tung.bquang@gmail.com>
2023-07-01 12:48:34 +0700

    Fix api gateway deployment

--- tutorial/serverless-basic-application/api-gateway.tf
+++ tutorial/serverless-basic-application/api-gateway.tf
@@ -41,7 +41,12 @@ resource "aws_api_gateway_deployment" "example" {
   rest_api_id = aws_api_gateway_rest_api.lambda.id
 
   triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.lambda.body))
+    # redeployment = sha1(jsonencode(aws_api_gateway_rest_api.lambda.body))
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.resource.id,
+      # aws_api_gateway_method.example.id,
+      aws_api_gateway_integration.integration.id,
+    ]))
   }
 
   lifecycle {
@@ -52,7 +57,7 @@ resource "aws_api_gateway_deployment" "example" {
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.lambda.id
   resource_id             = aws_api_gateway_resource.resource.id
-  http_method = "POST"
+  http_method             = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.hello_world.invoke_arn
 }
