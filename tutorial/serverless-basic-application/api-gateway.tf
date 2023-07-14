302e2ae79d7fc6845096b80cc179377e5a743565
anujpandey0212 <MS20BTECH11003@iith.ac.in>
2023-07-14 19:23:07 +0530

    All the variables mentioned in the issue #172 has been updated

--- tutorial/serverless-basic-application/api-gateway.tf
+++ tutorial/serverless-basic-application/api-gateway.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "lambda" {
 }
 
 # Resource
-resource "aws_api_gateway_resource" "resource" {
+resource "aws_api_gateway_resource" "app_resource" {
   path_part   = "resource"
   parent_id   = aws_api_gateway_rest_api.lambda.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.lambda.id
@@ -21,7 +21,7 @@ resource "aws_api_gateway_method" "method" {
 }
 
 # Integration
-resource "aws_api_gateway_integration" "integration" {
+resource "aws_api_gateway_integration" "app_integration" {
   rest_api_id             = aws_api_gateway_rest_api.lambda.id
   resource_id             = aws_api_gateway_resource.resource.id
   http_method             = aws_api_gateway_method.method.http_method
@@ -31,7 +31,7 @@ resource "aws_api_gateway_integration" "integration" {
 }
 
 # Deployment
-resource "aws_api_gateway_deployment" "example" {
+resource "aws_api_gateway_deployment" "app_deployment" {
   rest_api_id = aws_api_gateway_rest_api.lambda.id
 
   triggers = {
