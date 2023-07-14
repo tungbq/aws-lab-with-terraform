f14349a401332f53274b0a8e62bcfdec8cb58132
anujpandey0212 <MS20BTECH11003@iith.ac.in>
2023-07-14 19:29:04 +0530

    one was left did thatb too

--- tutorial/serverless-basic-application/api-gateway.tf
+++ tutorial/serverless-basic-application/api-gateway.tf
@@ -13,7 +13,7 @@ resource "aws_api_gateway_resource" "app_resource" {
 }
 
 # Method
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_method" "app_method" {
   rest_api_id   = aws_api_gateway_rest_api.lambda.id
   resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "POST"
