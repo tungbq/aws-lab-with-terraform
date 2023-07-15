c3347efa1dd7002a0ee24ad4c1ef7eff5ca9c02e
anujpandey0212 <MS20BTECH11003@iith.ac.in>
2023-07-15 11:29:05 +0530

    did the suggested changes

--- tutorial/serverless-basic-application/api-gateway.tf
+++ tutorial/serverless-basic-application/api-gateway.tf
@@ -15,7 +15,7 @@ resource "aws_api_gateway_resource" "app_resource" {
 # Method
 resource "aws_api_gateway_method" "app_method" {
   rest_api_id   = aws_api_gateway_rest_api.lambda.id
-  resource_id   = aws_api_gateway_resource.resource.id
+  resource_id   = aws_api_gateway_resource.app_resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
@@ -23,8 +23,8 @@ resource "aws_api_gateway_method" "app_method" {
 # Integration
 resource "aws_api_gateway_integration" "app_integration" {
   rest_api_id             = aws_api_gateway_rest_api.lambda.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
+  resource_id             = aws_api_gateway_resource.app_resource.id
+  http_method             = aws_api_gateway_method.app_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.hello_world.invoke_arn
@@ -36,9 +36,9 @@ resource "aws_api_gateway_deployment" "app_deployment" {
 
   triggers = {
     redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.resource.id,
-      aws_api_gateway_method.method.id,
-      aws_api_gateway_integration.integration.id,
+      aws_api_gateway_resource.app_resource.id,
+      aws_api_gateway_method.app_method.id,
+      aws_api_gateway_integration.app_integration.id,
     ]))
   }
 
@@ -50,7 +50,7 @@ resource "aws_api_gateway_deployment" "app_deployment" {
 # Stage
 resource "aws_api_gateway_stage" "lambda" {
   rest_api_id    = aws_api_gateway_rest_api.lambda.id
-  deployment_id = aws_api_gateway_deployment.example.id
+  deployment_id = aws_api_gateway_deployment.app_deployment.id
   stage_name            = "serverless_lambda_stage"
 
   # Temporarity disable logging. TODO: work on it later
