c6991d3c2ae651a2f6213a7334638bf1e8d92ba3
Tung Bui <tung.bquang@gmail.com>
2023-07-30 20:49:15 +0700

    i182: move serverless example to hands-on

---
+++ hands-on/serverless/lambda_http.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+
+import boto3
+import json
+
+print('Loading function')
+
+
+def lambda_handler(event, context):
+    '''Provide an event that contains the following keys:
+
+      - operation: one of the operations in the operations dict below
+      - tableName: required for operations that interact with DynamoDB
+      - payload: a parameter to pass to the operation being performed
+    '''
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    operation = event['operation']
+
+    if 'tableName' in event:
+        dynamo = boto3.resource('dynamodb').Table(event['tableName'])
+
+    operations = {
+        'create': lambda x: dynamo.put_item(**x),
+        'read': lambda x: dynamo.get_item(**x),
+        'update': lambda x: dynamo.update_item(**x),
+        'delete': lambda x: dynamo.delete_item(**x),
+        'list': lambda x: dynamo.scan(**x),
+        'echo': lambda x: x,
+        'ping': lambda x: 'pong'
+    }
+
+    if operation in operations:
+        return operations[operation](event.get('payload'))
+    else:
+        raise ValueError('Unrecognized operation "{}"'.format(operation))
--- serverless/README.md
+++
@@ -1,6 +0,0 @@
-# Architecture:
-[User] -> [API_Gateway] -> [Lambda] -> [DynamoDB]
-
-# References:
-## https://developer.hashicorp.com/terraform/tutorials/aws/lambda-api-gateway
-## https://dev.to/rolfstreefkerk/openapi-with-terraform-on-aws-api-gateway-17je
