0238d39622a99c1a7b6cd1a9a0f56b2c5488b945
Tung Bui <tung.bquang@gmail.com>
2023-08-12 21:06:17 +0700

    focalboard: fix nginx issue

--- selfhosted/focalboard/simple-focalboard/scripts/install_app.sh
+++ selfhosted/focalboard/simple-focalboard/scripts/install_app.sh
@@ -13,53 +13,52 @@ apt install -y nginx
 # nano /etc/nginx/sites-available/focalboard
 cat <<EOF >/etc/nginx/sites-available/focalboard
 upstream focalboard {
-     server localhost:8000;
-     keepalive 32;
-  }
+    server localhost:8000;
+    keepalive 32;
+}
 
-  server {
+server {
     listen 80 default_server;
+    server_name focalboard.example.com;
 
-   server_name focalboard.example.com;
+    location ~ /ws/.* {
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+        client_max_body_size 50M;
+        proxy_set_header Host $http_host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Frame-Options SAMEORIGIN;
+        proxy_buffers 256 16k;
+        proxy_buffer_size 16k;
+        client_body_timeout 60;
+        send_timeout 300;
+        lingering_timeout 5;
+        proxy_connect_timeout 1d;
+        proxy_send_timeout 1d;
+        proxy_read_timeout 1d;
+        proxy_pass http://focalboard;
+    }
 
-    location ~ /ws/* {
-       proxy_set_header Upgrade $http_upgrade;
-       proxy_set_header Connection "upgrade";
-       client_max_body_size 50M;
-       proxy_set_header Host $http_host;
-       proxy_set_header X-Real-IP $remote_addr;
-       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-       proxy_set_header X-Forwarded-Proto $scheme;
-       proxy_set_header X-Frame-Options SAMEORIGIN;
-       proxy_buffers 256 16k;
-       proxy_buffer_size 16k;
-       client_body_timeout 60;
-       send_timeout 300;
-       lingering_timeout 5;
-       proxy_connect_timeout 1d;
-       proxy_send_timeout 1d;
-       proxy_read_timeout 1d;
-       proxy_pass http://focalboard;
-   }
-
-   location / {
-       client_max_body_size 50M;
-       proxy_set_header Connection "";
-       proxy_set_header Host $http_host;
-       proxy_set_header X-Real-IP $remote_addr;
-       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-       proxy_set_header X-Forwarded-Proto $scheme;
-       proxy_set_header X-Frame-Options SAMEORIGIN;
-       proxy_buffers 256 16k;
-       proxy_buffer_size 16k;
-       proxy_read_timeout 600s;
-       proxy_cache_revalidate on;
-       proxy_cache_min_uses 2;
-       proxy_cache_use_stale timeout;
-       proxy_cache_lock on;
-       proxy_http_version 1.1;
-       proxy_pass http://focalboard;
-   }
+    location / {
+        client_max_body_size 50M;
+        proxy_set_header Connection "";
+        proxy_set_header Host $http_host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Frame-Options SAMEORIGIN;
+        proxy_buffers 256 16k;
+        proxy_buffer_size 16k;
+        proxy_read_timeout 600s;
+        proxy_cache_revalidate on;
+        proxy_cache_min_uses 2;
+        proxy_cache_use_stale timeout;
+        proxy_cache_lock on;
+        proxy_http_version 1.1;
+        proxy_pass http://focalboard;
+    }
 }
 EOF
 
@@ -93,5 +92,5 @@ systemctl start focalboard.service
 systemctl enable focalboard.service
 
 # Checking
-curl localhost:8080
+curl localhost:8000
 curl localhost
