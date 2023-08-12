0cb4c081f6aebc2b3b869d028facf98d15f0450b
Tung Bui <tung.bquang@gmail.com>
2023-08-12 17:12:08 +0700

    i217: fix egress tls

--- selfhosted/focalboard/simple-focalboard/main.tf
+++ selfhosted/focalboard/simple-focalboard/main.tf
@@ -40,4 +40,12 @@ resource "aws_security_group" "focalboard_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
