b96170f8396b28b481c7d8af631c1aa77a991005
Tung Bui <tung.bquang@gmail.com>
2023-08-12 16:42:49 +0700

    focalboard: init ec2 instance

--- selfhosted/focalboard/simple-focalboard/main.tf
+++ selfhosted/focalboard/simple-focalboard/main.tf
@@ -1,1 +1,13 @@
-# TODO
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "focalboard_instance" {
+  # To get the AMI ID, visit: https://us-east-1.console.aws.amazon.com/ec2/home?region=us-east-1#AMICatalog
+  ami           = "ami-0a0c8eebcdd6dcbd0" # Ubuntu 22.04
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "focalboard-instance"
+  }
+}
