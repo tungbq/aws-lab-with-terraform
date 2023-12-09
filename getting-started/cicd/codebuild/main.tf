964fd4b287e68430c879a21f878dc40efb73bc59
Tung Bui <tung.bquang@gmail.com>
2023-12-09 12:31:41 +0700

    output artifacts to S3

--- getting-started/cicd/codebuild/main.tf
+++ getting-started/cicd/codebuild/main.tf
@@ -111,7 +111,8 @@ resource "aws_codebuild_project" "demo_project" {
   service_role = aws_iam_role.demo_codebuild.arn
 
   artifacts {
-    type = "NO_ARTIFACTS"
+    type = "S3"
+    location = aws_s3_bucket.demo_aws_codebuild_bucket_output.id
   }
 
   environment {
