aa34a0df4088f7f2e3a80f115bb517cac1afc487
Tung Bui <tung.bquang@gmail.com>
2023-12-13 07:53:34 +0700

    clean code for ec2

--- getting-started/cicd/codepipeline/modules/ec2_codedeploy/variables.tf
+++ getting-started/cicd/codepipeline/modules/ec2_codedeploy/variables.tf
@@ -2,3 +2,15 @@ variable "profile_name" {
   description = "Name of the IAM profile"
   type        = string
 }
+
+variable "ec2_tag_name" {
+  description = "Name of the EC2 tag"
+  type        = string
+  default     = "MyCodePipelineDemo"
+}
+
+variable "ec2_codedeploy_sg_name" {
+  description = "ec2-codedeploy-security-group"
+  type        = string
+  default     = "ec2-codedeploy-security-group"
+}
