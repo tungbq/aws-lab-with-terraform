0bae48a6a0ba6b68e56c74755469d1428ab1bd19
Tung Bui <tung.bquang@gmail.com>
2024-03-14 19:16:26 +0700

    core: RDS updates

--- getting-started/rds/create-simple-rds/main.tf
+++ getting-started/rds/create-simple-rds/main.tf
@@ -21,4 +21,14 @@ resource "aws_db_instance" "basic_rds_instance" {
   db_name           = "my_rds_instance"
   username          = "root"
   password          = "password"
+
+  tags = {
+    Name        = "MyRDSInstance" # Adding tags for better organization
+    Environment = "Dev"           # Example tag, adjust as needed
+  }
+
+  # Optional: Configure backup retention period, multi AZ deployment, and deletion protection
+  backup_retention_period = 7     # Retain backups for 7 days
+  multi_az                = false # Enable multi-AZ deployment for high availability
+  deletion_protection     = true  # Prevent accidental deletion of the RDS instance
 }
